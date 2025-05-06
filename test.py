e0fd6794f93ab427aae5c180c890559c396728a0
Seymour <seymourontroad@gmail.com>
2025-05-06 21:50:34 +0800

    在vscode中练习git和源码管理器

---
+++ test.py
@@ -0,0 +1,14 @@
+"""
+Author: SeyMour2SeeMore
+Date: 2025-05-06 21:44:27
+LastEditors: SeyMour2SeeMore
+LastEditTime: 2025-05-06 21:44:38
+FilePath: /test_repo/test.py
+Description: test在vscode中使用源码管理器和git
+
+Copyright (c) 2025 by ${seymourontroad@gmail.com}, All Rights Reserved.
+"""
+
+
+if __name__ == '__main__':
+    print('Hello, World!')
